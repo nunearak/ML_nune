171eea04167d679b59a2537f4eae9a99544bd709
nunearak <nune.arakelyan@tumo.org>
2023-06-28 16:53:31 +0100

    Add more charts

---
+++ Session1/Pentigral.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import random
+
+random.seed(42)
+data=[random.random() for _ in range (1000)]
+plt.hist (data, bins = 30, color ="skyblue", alpha = 0.7)
+plt.xlabel("Value")
+plt.ylabel("Frequenc")
+plt.title("Histogram")
+plt.show()
+        
+
+
