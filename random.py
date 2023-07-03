488bc004615ce07f2592af6bc299f5bac8d97b3f
nunearak <nune.arakelyan@tumo.org>
2023-07-03 17:00:22 +0100

    session 3

---
+++ random.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+random_array = np.random.randint(0, 100, size = 10)
+print("The array:", random_array)
+
+mean = np.mean(random_array)
+print("Mean: ", mean)
+
+max_val = np.max(random_array)
+print ("Max: ", max_val)
+
+min_val = np.min(random_array)
+print ("Min: ", min_val)
+
+sorted_array = np.sort(random_array)
+print ("Sorted array :", shuffled_array)
+
+squared_array = np.square (random_array)
+print("Squared array:", squared_array)
+
+sqrt_array = np.sqrt(random_array)
+print ("Square root array:", sqrt_array)
